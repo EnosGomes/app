9ea4d8d5bd794cfc27d2efd5170f4c44979f9719
EnosGomes <enoskratos@hotmail.com>
2023-03-31 23:42:15 -0300

    two way databinding simples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './usuario/usuario/usuario.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { UsuarioComponent } from './usuario/usuario/usuario.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/usuario/usuario/usuario.component.ts
+++ src/app/usuario/usuario/usuario.component.ts
@@ -7,11 +7,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class UsuarioComponent implements OnInit {
   @Input() filho : any;
+  twoway: string = "";
+
   constructor() { }
+
   ngOnInit(): void { }
+
   botaoClicado(){
     alert("botao clicado")
   }
+
   onKeyUp(evento : any){
     console.log(evento.target.value)
   }
